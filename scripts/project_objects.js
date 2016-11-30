ce8f895fa53654b6aac76f95f6f0bc6f1ffb6466
irvinemd55 <irvinedowning@gmail.com>
2016-11-30 11:30:47 -0800

    added javascript modifier to projects section

---
+++ scripts/project_objects.js
@@ -0,0 +1,27 @@
+'use strict';
+var projectObjects = [
+  {
+    title:       'Busmall',
+    category:    '201',
+    author:      'Irvine Downing',
+    authorUrl:   'https://github.com/irvinemd55/busmall',
+    publishedOn: '2016-10-10',
+    body:        '<p>201 Project covering busmall app</p>'
+  },
+  {
+    title:       'Cookie Stand',
+    category:    '201',
+    author:      'Irvine Downing',
+    authorUrl:   'https://github.com/irvinemd55/busmall',
+    publishedOn: '2016-09-29',
+    body:        '<p>201 Project creating a cookie stand app</p>'
+  },
+  {
+    title:       'Mobile First',
+    category:    '301',
+    author:      'Irvine Downing',
+    authorUrl:   'https://github.com/irvinemd55/01-mobile-first',
+    publishedOn: '2016-10-24',
+    body:        '<p>301 Project modifying a website for mobile access</p>'
+  },
+];
