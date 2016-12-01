6a2de7816e8220fdb0c658950e88c317f675b4c5
irvinemd55 <irvinedowning@gmail.com>
2016-12-01 11:24:12 -0800

    additional view functionality added

---
+++ scripts/portfolio_view.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var portfolioView = {};
+
+portfolioView.handleMainNav = function () {
+  $('.main-nav').on('click', '.tab', function() {
+    $('.tab-content').hide();
+    $('#' + $(this).data('content')).fadeIn();
+
+
+  });
+  $('.main-nav .tab:first').click();
+};
+
+portfolioView.handleMainNav();
